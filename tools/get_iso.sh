007b87d6239f5666771ebffe129229218d6a7371
Sergey Berstnev <tviks879@gmail.com>
2021-10-12 10:04:30 +0300

    fix download script

--- tools/get_iso.sh
+++ tools/get_iso.sh
@@ -9,13 +9,15 @@ echo $iso
 if [[ "$iso" == "ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz" ]]; then
   MESSAGE="ISO DOWNLOADED" ; green_echo 
   MESSAGE="UNZIPING" ; blue_echo
-  unzip iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz -d iso/
+  sudo unxz iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz
+  sudo mv iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img iso/
   MESSAGE="UNZIPING COMPLETE" ; green_echo
 else
   MESSAGE="DOWLOAD ISO, PLEASE WAIT" ; blue_echo
-  wget -P iso/download/ https://cdimage.ubuntu.com/releases/20.04.3/release/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz
+  wget -nc https://cdimage.ubuntu.com/releases/20.04.3/release/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz -P iso/download/
   MESSAGE="UNZIPING" ; blue_echo
-  unzip iso/download/2021-05-07-raspios-buster-armhf-lite.zip -d iso/
+  sudo unxz iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz
+  sudo mv iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img iso/
   MESSAGE="UNZIPING COMPLETE" ; green_echo
 fi
 
