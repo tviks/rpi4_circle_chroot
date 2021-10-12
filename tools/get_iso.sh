aa82b0380ff6594658642360351e8d5330c3f7fd
Sergey Berstnev <tviks879@gmail.com>
2021-10-12 09:42:11 +0300

    add readmd

--- tools/get_iso.sh
+++ tools/get_iso.sh
@@ -6,14 +6,14 @@ iso=$(ls iso/download/ | grep 2021-05-07-raspios-buster-armhf-lite.zip)
 
 echo $iso
 
-if [[ "$iso" == "2021-05-07-raspios-buster-armhf-lite.zip" ]]; then
+if [[ "$iso" == "ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz" ]]; then
   MESSAGE="ISO DOWNLOADED" ; green_echo 
   MESSAGE="UNZIPING" ; blue_echo
-  unzip iso/download/2021-05-07-raspios-buster-armhf-lite.zip -d iso/
+  unzip iso/download/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz -d iso/
   MESSAGE="UNZIPING COMPLETE" ; green_echo
 else
   MESSAGE="DOWLOAD ISO, PLEASE WAIT" ; blue_echo
-  wget -P iso/download/ https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-05-28/2021-05-07-raspios-buster-armhf-lite.zip
+  wget -P iso/download/ https://cdimage.ubuntu.com/releases/20.04.3/release/ubuntu-20.04.3-preinstalled-server-arm64+raspi.img.xz
   MESSAGE="UNZIPING" ; blue_echo
   unzip iso/download/2021-05-07-raspios-buster-armhf-lite.zip -d iso/
   MESSAGE="UNZIPING COMPLETE" ; green_echo
